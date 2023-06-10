bba3f2cd771099d8afd06daae3ecee7351c43190
TheDeCOderP <113170140+TheDeCOderP@users.noreply.github.com>
2023-06-10 10:00:50 +0000

    commit

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import './globals.css';
+import Head from 'next/head';
+
+
+
+
+export default function MyApp({ Component, pageProps }) {
+    <Head>
+  <link rel="icon" href="/favicon.ico" />
+</Head>
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import banner from './ramsnehi-plywood.jpg';
+import Head from 'next/head';
+
+export default function Home() {
+  
+return (
+
+    
+
+    <>
+
+
+
+<Head>
+       <link rel="icon" href="/favicon.ico" />
+        <title>Ramsnehi Plywood & Hardware</title>
+        <meta name="description" content="Discover a diverse selection of top-quality plywood materials, captivating sunmica sheets, and an extensive range of hardware products at competitive prices." />
+        <meta name="keywords" content="Ramsnehi Plywood, Plywood shop Mandsaur, Plywood Shop Near Me, Hardware store Mandsaur, Plywood suppliers Mandsaur, Plywood dealer Mandsaur, Hardware shop near me, Plywood and hardware Mandsaur, Quality plywood Mandsaur, Sunmica, Affordable hardware Mandsaur, Plywood sheets Mandsaur, Door hardware Mandsaur, Plywood cutting service Mandsaur, Cabinet hardware Mandsaur, Plywood for furniture Mandsaur, Hardware fittings Mandsaur, Best plywood shop Mandsaur" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+    <section>
+      <h1 className='banner_heading'>Welcome to Ramsnehi Plywood</h1>
+      <Image src={banner} alt="Ramsnehi Plywood" className="banner_image" />
+    </section>
+    </>
+  );
+}
