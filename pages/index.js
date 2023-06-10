029d540b86a038427483c23baa73b21240e46ac9
TheDeCOderP <113170140+TheDeCOderP@users.noreply.github.com>
2023-06-10 10:04:55 +0000

    commit

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ return (
        <link rel="icon" href="/favicon.ico" />
         <title>Ramsnehi Plywood & Hardware</title>
         <meta name="description" content="Discover a diverse selection of top-quality plywood materials, captivating sunmica sheets, and an extensive range of hardware products at competitive prices." />
-        <meta name="keywords" content="Ramsnehi Plywood, Plywood shop Mandsaur, Plywood Shop Near Me, Hardware store Mandsaur, Plywood suppliers Mandsaur, Plywood dealer Mandsaur, Hardware shop near me, Plywood and hardware Mandsaur, Quality plywood Mandsaur, Sunmica, Affordable hardware Mandsaur, Plywood sheets Mandsaur, Door hardware Mandsaur, Plywood cutting service Mandsaur, Cabinet hardware Mandsaur, Plywood for furniture Mandsaur, Hardware fittings Mandsaur, Best plywood shop Mandsaur" />
+        <meta name="keywords" content="Plywood , Ramsnehi Plywood, Plywood shop Mandsaur, Plywood Shop Near Me, Hardware store Mandsaur, Plywood suppliers Mandsaur, Plywood dealer Mandsaur, Hardware shop near me, Plywood and hardware Mandsaur, Quality plywood Mandsaur, Sunmica, Affordable hardware Mandsaur, Plywood sheets Mandsaur, Door hardware Mandsaur, Plywood cutting service Mandsaur, Cabinet hardware Mandsaur, Plywood for furniture Mandsaur, Hardware fittings Mandsaur, Best plywood shop Mandsaur" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
